4e8704052059ab332580c99a482de353297b64b0
lin <zx@lendata.net>
2023-03-22 14:07:39 +0800

    更新response 新增解密和验签名错误方法

--- gf2/response/error.go
+++ gf2/response/error.go
@@ -9,28 +9,28 @@ import (
 )
 
 func InternalError(ctx context.Context, ext interface{}) error {
-	return gerror.NewCode(gcode.New(500, g.I18n().Translate(ctx, "InternalError"), ext))
+	return Error(500, g.I18n().Translate(ctx, "InternalError"), ext)
 }
 func UnAuthorizedError(ctx context.Context, ext interface{}) error {
-	return gerror.NewCode(gcode.New(401, g.I18n().Translate(ctx, "UnAuthorized"), ext))
+	return Error(401, g.I18n().Translate(ctx, "UnAuthorized"), ext)
 }
 func DecryptError(ctx context.Context, ext interface{}) error {
-	return gerror.NewCode(gcode.New(402, g.I18n().Translate(ctx, "DecryptFailed"), ext))
+	return Error(402, g.I18n().Translate(ctx, "DecryptFailed"), ext)
 }
 func SignatureError(ctx context.Context, ext interface{}) error {
-	return gerror.NewCode(gcode.New(403, g.I18n().Translate(ctx, "SignatureError"), ext))
+	return Error(403, g.I18n().Translate(ctx, "SignatureError"), ext)
 }
 func NotFoundError(ctx context.Context, ext interface{}) error {
-	return gerror.NewCode(gcode.New(404, g.I18n().Translate(ctx, "NotFound"), ext))
+	return Error(404, g.I18n().Translate(ctx, "NotFound"), ext)
 }
 
-// StandardError 通用错误返回，支持i18n 如需自定义错误码ENUM对应相关错误，请自行参考实现
-func StandardError(ctx context.Context, code int, message string, trans bool, ext interface{}) error {
-	if trans {
-		message = g.I18n().Translate(ctx, message)
-	}
-	if code == 401 {
-		return UnAuthorizedError(ctx, ext)
-	}
+// I18nError 返回一个标准错误信息，适用i18n 如需自定义错误码ENUM对应相关错误，请自行参考实现
+func I18nError(ctx context.Context, code int, message string, ext interface{}) error {
+	message = g.I18n().Translate(ctx, message)
+	return Error(code, message, ext)
+}
+
+// Error 返回一个标准错误信息  code 建议使用400通用错误码（或自定义四位错误码与HTTP状态码区分开），一般不使用公共的401,402,403,404错误码，除非明确输出的是相关内容
+func Error(code int, message string, ext interface{}) error {
 	return gerror.NewCode(gcode.New(code, message, ext))
 }
--- gf2/response/json.go
+++ gf2/response/json.go
@@ -3,12 +3,10 @@ package response
 import (
 	"context"
 	"os"
-	"strings"
 
 	"github.com/gogf/gf/v2/crypto/gmd5"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/i18n/gi18n"
-	"github.com/gogf/gf/v2/os/gctx"
 	"github.com/gogf/gf/v2/os/gtime"
 )
 
@@ -24,13 +22,13 @@ func (rec *json) Success(ctx context.Context, data interface{}) {
 	rec.Writer(ctx, data, "Success", 200, 0, nil)
 }
 
+func (rec *json) InternalError(ctx context.Context, message string) {
+	rec.Writer(ctx, nil, message, 500, 500, nil)
+}
+
 // Error errCode 建议使用400通用错误码（或自定义四位错误码与HTTP状态码区分开），一般不使用公共的401,402,403,404错误码，除非明确输出的是相关内容
 func (rec *json) Error(ctx context.Context, message string, errCode int, ext interface{}) {
 	rec.Writer(ctx, nil, message, 400, errCode, ext)
-}
-
-func (rec *json) InternalError(ctx context.Context, message string) {
-	rec.Writer(ctx, nil, message, 500, 500, nil)
 }
 func (rec *json) UnAuthorizedError(ctx context.Context, message string, ext interface{}) {
 	rec.Writer(ctx, nil, message, 401, 401, ext)
@@ -46,18 +44,13 @@ func (rec *json) NotFoundError(ctx context.Context, message string) {
 }
 
 type JsonFormat struct {
-	Code         int         `json:"code"`         // 业务码，用于业务判断（首选），固定 200/400/401/404/500，与http状态码同步，通常取该值判断是否有错误需要处理
-	ErrorCode    int         `json:"errorCode"`    // 错误码，用于业务判断（可选），-1/400(通用错误)/51(参数验证错误)/401/404/500/other，通常忽略该值，除非业务需要判断详细错误类型（例：交易场景，交易失败返回400业务码时，返回余额不足、账户冻结等详细错误码用于后续业务处理）
-	Message      interface{} `json:"message"`      // 消息，业务码非400时固定输出 Success/Authorization/NotFound/InternalError中的一种，400时输出详细错误（可能含i18n转译，建议仅展示或记录信息，不可用于业务判断）
-	Data         interface{} `json:"data"`         // 返回数据
-	TraceId      interface{} `json:"traceId"`      // 请求唯一追踪ID,用于日志定位
-	Server       string      `json:"server"`       // 服务端 host name md5 值
-	Runtime      int64       `json:"runtime"`      // 当前任务执行耗时（ms）
-	Lang         string      `json:"lang"`         // 当前上下文语言
-	Ext          interface{} `json:"ext"`          // 拓展数据（可能含有多个错误详情或其他附加数据，例：复杂登录场景下的401错误返回具体登录地址）
-	BuildVersion interface{} `json:"buildVersion"` // 当前程序运行版本号
-	BootTime     int64       `json:"bootTime"`     // 系统启动时长（s）（使用时间戳差值而不是time.sub进行计算，time.sub受系统单调时钟策略影响，部分系统如mac休眠后单调时钟会停止，导致计算产生偏差）
-	BootAt       string      `json:"bootAt"`       // 系统启动时间（应用启动时初始化）
+	Code      int         `json:"code"`      // 业务码，用于业务判断（首选），固定 200/400/401/404/500，与http状态码同步，通常取该值判断是否有错误需要处理
+	ErrorCode int         `json:"errorCode"` // 错误码，用于业务判断（可选），-1/400(通用错误)/51(参数验证错误)/401/404/500/other，通常忽略该值，除非业务需要判断详细错误类型（例：交易场景，交易失败返回400业务码时，返回余额不足、账户冻结等详细错误码用于后续业务处理）
+	Message   interface{} `json:"message"`   // 消息，业务码非400时固定输出 Success/Authorization/NotFound/InternalError中的一种，400时输出详细错误（可能含i18n转译，建议仅展示或记录信息，不可用于业务判断）
+	Data      interface{} `json:"data"`      // 返回数据
+	Runtime   int64       `json:"runtime"`   // 当前任务执行耗时（ms）
+	Lang      string      `json:"lang"`      // 当前上下文语言
+	Ext       interface{} `json:"ext"`       // 拓展数据（可能含有多个错误详情或其他附加数据，例：复杂登录场景下的401错误返回具体登录地址）
 }
 
 // Writer 标准格式数据输出
@@ -66,22 +59,18 @@ func (rec *json) Writer(ctx context.Context, data interface{}, message string, c
 	r.Response.WriteStatus(code)
 	r.Response.ClearBuffer()
 	serverName, _ := os.Hostname()
-	serverName, _ = gmd5.Encrypt(serverName)
+	serverId, _ := gmd5.Encrypt(serverName)
 	r.Response.WriteJson(JsonFormat{
-		Code:         code,
-		Message:      message,
-		Data:         data,
-		TraceId:      gctx.CtxId(ctx),
-		Server:       strings.ToUpper(serverName),
-		Ext:          ext,
-		Lang:         gi18n.LanguageFromCtx(ctx),
-		ErrorCode:    errCode,
-		Runtime:      gtime.Now().TimestampMilli() - r.EnterTime,
-		BuildVersion: r.GetCtxVar("BUILD_VERSION", "UNKNOWN"),
-		BootTime:     GetBootTime(),
-		BootAt:       GetBootAt(),
+		Code:      code,
+		Message:   message,
+		Data:      data,
+		Ext:       ext,
+		Lang:      gi18n.LanguageFromCtx(ctx),
+		ErrorCode: errCode,
+		Runtime:   gtime.Now().TimestampMilli() - r.EnterTime,
 	})
 	r.Response.Header().Set("Content-Type", "application/json;charset=utf-8")
+	r.Response.Header().Set("Server-Id", serverId)
 	r.ExitAll()
 }
 
