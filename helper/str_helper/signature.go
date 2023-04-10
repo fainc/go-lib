c321a1b527bc2cd6fd908cbf79171c6505b7cfb3
lin <zx@lendata.net>
2023-04-10 14:53:19 +0800

    更新 sign方法

--- helper/str_helper/signature.go
+++ helper/str_helper/signature.go
@@ -1,6 +1,7 @@
 package str_helper
 
 import (
+	"encoding/json"
 	"fmt"
 	"sort"
 	"strings"
@@ -24,6 +25,38 @@ func SignatureStr(m map[string]string, keyOptions *SignatureStrKeyOptions, exclu
 		if _, ok := m[k]; ok {
 			if exclude == nil || !StrInSlice(k, exclude) {
 				str = fmt.Sprintf("%s&%s=%s", str, k, m[k])
+			}
+		}
+	}
+	if keyOptions != nil && keyOptions.Key != "" && keyOptions.Value != "" {
+		str = fmt.Sprintf("%s&%s=%s", str, keyOptions.Key, keyOptions.Value)
+	}
+	str = strings.TrimPrefix(str, "&")
+	return
+}
+
+// SignatureComplexStr 常用签名处理方法 map转签名串（字典升序），适用多层的复杂数据
+func SignatureComplexStr(m map[string]string, keyOptions *SignatureStrKeyOptions, exclude []string) (str string) {
+	var keys []string
+	for k := range m {
+		keys = append(keys, k)
+	}
+	// 按字典升序排列
+	sort.Strings(keys)
+	str = ""
+	for _, k := range keys {
+		if _, ok := m[k]; ok {
+			v := m[k]
+			is := json.Valid([]byte(v)) // 判断是否JSON
+			if is {
+				var vj map[string]string
+				err := json.Unmarshal([]byte(v), &vj) // 尝试解析json
+				if err != nil {
+					v = SignatureComplexStr(vj, keyOptions, exclude)
+				}
+			}
+			if exclude == nil || !StrInSlice(k, exclude) {
+				str = fmt.Sprintf("%s&%s=%s", str, k, v)
 			}
 		}
 	}
